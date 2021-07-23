71e95e61f8ffe6c8a0869735586f4781363938f4
yoshiyu1209 <86815449+yoshiyu1209@users.noreply.github.com>
2021-07-23 18:15:59 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+
+    with_options format:{with: /\A[ぁ-んァ-ン一-龥]/} do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format:{with: /\A[ァ-ヶ一-]+\z/} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+
+    validates :birthday
+
+  end
+
+  # VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,}\z/
+  #     validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'items#index'
-
+  resources :users, only: [:edit, :update]
   resources :items
 end
