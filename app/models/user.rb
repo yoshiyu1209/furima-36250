029dafb54fb5120341d04dd95a070fa63bd896e8
yoshiyu1209 <86815449+yoshiyu1209@users.noreply.github.com>
2021-07-24 21:07:53 +0900

    テストコード修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
 
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' } do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
       validates :last_name
       validates :first_name
     end
