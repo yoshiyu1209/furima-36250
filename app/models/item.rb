027d1b769467d03517cc07761c82a34620ef4ea5
yoshiyu1209 <86815449+yoshiyu1209@users.noreply.github.com>
2021-07-26 12:51:16 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :product_status
+  belongs_to :shipping_charges
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+  
 
 end
