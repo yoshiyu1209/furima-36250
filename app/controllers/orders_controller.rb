65e675fbf82c0f753907cda7ed8ef5224ff12eca
yoshiyu1209 <86815449+yoshiyu1209@users.noreply.github.com>
2021-08-01 19:13:54 +0900

    エラー表示設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
 
   def index
