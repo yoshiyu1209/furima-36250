78a2a30644daf147ab2a972d2243bfdd014a91f1
yoshiyu1209 <86815449+yoshiyu1209@users.noreply.github.com>
2021-08-01 18:30:56 +0900

    フォームの情報を保存設定

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -13,6 +13,6 @@ class ItemOrder
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order_id)
+    Address.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id,)
   end
 end
