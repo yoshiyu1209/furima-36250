5b51f4866b75f0b7d5940c5b5312d3f326060039
yoshiyu1209 <86815449+yoshiyu1209@users.noreply.github.com>
2021-08-02 16:07:17 +0900

    販売済みの商品にsoldout表示設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @item_order = ItemOrder.new
@@ -24,16 +25,21 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:item_order).permit(:postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+    params.require(:item_order).permit(:postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number).merge(
+      token: params[:token], user_id: current_user.id, item_id: params[:item_id]
+    )
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
       currency: 'jpy'
     )
   end
-  
+
+  def move_to_index
+    redirect_to root_path unless current_user.id != @item.user.id && @item.order.nil?
+  end
 end
--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,6 +1,7 @@
 class ItemOrder
   include ActiveModel::Model
-  attr_accessor :token, :postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :token, :postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number, :user_id,
+                :item_id
 
   with_options presence: true do
     validates :token, :municipality, :address
@@ -11,10 +12,10 @@ class ItemOrder
     validates :item_id
   end
   validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' }
-  
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id,)
+    Address.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address,
+                   building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
