52bd52f4f4d64f6bafa5365d717d2e03ed74c8d4
yoshiyu1209 <86815449+yoshiyu1209@users.noreply.github.com>
2021-08-02 13:01:12 +0900

    決済機能実装とバリデーション設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
   def create
     @item_order = ItemOrder.new(order_params)
     if @item_order.valid?
+      pay_item
       @item_order.save
       redirect_to root_path(@item)
     else
@@ -23,8 +24,16 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:item_order).permit(:postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:item_order).permit(:postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
 
+  def pay_item
+    Payjp.api_key = "sk_test_5d075738467bc96027615fcc"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
   
 end
